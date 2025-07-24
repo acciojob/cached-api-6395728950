ebb2c57493c5e9c6b6088a162a778eea30368704
kuldeepparmar <kuldeep07122002@gmail.com>
2025-07-24 15:01:18 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
-
+ 
 const App = () => {
+  const[input,setInput]= useState([]);
+  const[loading,setLoading] = useState(true);
+  const[userId,setId] = useState(1);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    .then(data=>{
+      setInput(data);
+      setLoading(false);
+    })
+  },[])
+  console.log("what is in input",input);
+   const filterData = useMemo(()=>{
+     if(userId<1  || userId>10){
+         return input;
+     }
+        return input.filter((item)=>item.userId==userId);
+   },[userId,input]);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input type="text" value={userId} onChange={(e)=>setId(e.target.value)}></input>
+        {
+          loading ? <p>Loading...</p> :(
+             <ul>
+              {
+                filterData.map((item,index)=>(
+                   <li key={index}>
+                    <h1>{item.title}</h1>
+                    <p>{item.body}</p>
+                   </li>
+                ))
+              }
+             </ul>
+        ) 
+        }
     </div>
   )
 }
